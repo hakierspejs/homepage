1f8c0b9ed9ec406f1c5037c87cc5970573b7811f
Jacek Wielemborek <d33tah@gmail.com>
2020-06-17 20:02:41 +0200

    If there's no scheduled upcoming event, don't crash CI.

--- update_next_meeting.py
+++ update_next_meeting.py
@@ -2,9 +2,13 @@
 
 import meetupscraper
 import textwrap
+import sys
 
 next_events = meetupscraper.get_upcoming_events('Hakierspejs-Łódź')
-next_event = list(sorted(next_events, key=lambda x: x.date))[0]
+next_events = list(sorted(next_events, key=lambda x: x.date))
+if not next_events:
+    sys.exit()
+next_event = next_events[0]
 next_date = next_event.date.strftime("%d-%m-%Y %H:%M")
 s = textwrap.dedent(f'''
 {{% assign next_meeting = "{next_date}" %}}
@@ -13,9 +17,9 @@ s = textwrap.dedent(f'''
 '''.lstrip())
 
 if next_event.venue.name == 'Online event':
-    s += textwrap.dedent(f'''{{% comment %}}
-      {{% assign next_meeting_location = "" %}}
-    {{% endcomment %}}'''.strip())
+    s += textwrap.dedent('''{% comment %}
+      {% assign next_meeting_location = "" %}
+    {% endcomment %}'''.strip())
 else:
     s += f'{{% assign next_meeting_location = "{next_event.venue.name}" %}}'
 with open('./_includes/next_meeting.txt', 'w') as f:
