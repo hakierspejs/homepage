afce36aa1ed71cdbfbb1ce6f7f35d151f45c3897
thinkofher <mail@bdudek.xyz>
2021-08-07 19:54:49 +0200

    Add meetings.py script support

---
+++ scripts/meetings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import meetupscraper
+import textwrap
+import sys
+import json
+
+if __name__ == '__main__':
+    next_events = meetupscraper.get_upcoming_events('Hakierspejs-Łódź')
+    next_events = list(sorted(next_events, key=lambda x: x.date))
+
+    if not next_events:
+        sys.exit()
+
+    next_event = next_events[0]
+    next_date = next_event.date.strftime("%d-%m-%Y %H:%M")
+
+    json_data = {
+        'date': next_event.date.strftime("%d-%m-%Y %H:%M"),
+        'url': next_event.url,
+    }
+
+    json_data['location'] = ''
+    if next_event.venue.name != 'Online event':
+        json_data['location'] = next_event.venue.name
+
+    with open('./_data/meetings.json', 'w') as f:
+        f.write(json.dumps(json_data))
