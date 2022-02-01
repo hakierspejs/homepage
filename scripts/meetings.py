c58a5fd621ccb116e27bc05960cffb0ec1dbb133
Jacek Wielemborek <295322+d33tah@users.noreply.github.com>
2022-02-01 11:07:45 +0100

    Separately save live_meetings.json

--- scripts/meetings.py
+++ scripts/meetings.py
@@ -5,14 +5,8 @@ import textwrap
 import sys
 import json
 
-if __name__ == '__main__':
-    next_events = meetupscraper.get_upcoming_events('Hakierspejs-Łódź')
-    next_events = list(sorted(next_events, key=lambda x: x.date))
 
-    if not next_events:
-        sys.exit()
-
-    next_event = next_events[0]
+def save_event(next_event, fpath):
     next_date = next_event.date.strftime("%d-%m-%Y %H:%M")
 
     json_data = {
@@ -24,5 +18,17 @@ if __name__ == '__main__':
     if next_event.venue.name != 'Online event':
         json_data['location'] = next_event.venue.name
 
-    with open('./_data/meetings.json', 'w') as f:
+    with open(fpath, 'w') as f:
         f.write(json.dumps(json_data))
+
+if __name__ == '__main__':
+    next_events = meetupscraper.get_upcoming_events('Hakierspejs-Łódź')
+    next_events = list(sorted(next_events, key=lambda x: x.date))
+
+    if not next_events:
+        sys.exit()
+
+    next_event = next_events[0]
+    save_event(next_event, './_data/meetings.json')
+    next_live_event = [e for e in next_events if e.venue.name == 'Hakierspejs']
+    save_event(next_live_event, './_data/live_meetings.json')
