f31d5c43ffc38fa475d2559f2ff34088f971141b
Kacper Sagalara <mail@kasesag.me>
2024-12-17 11:02:39 +0100

    Update dl-apply.sh script.

--- scripts/dl-apply.sh
+++ scripts/dl-apply.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # 
-# Downloads an article about applying for a membership
-# from HSŁ wiki and converts it to the format understood
-# by jekyll. Requires wget and bash installed. 
+# Downloads 2 articles from HSŁ wiki and converts them to the 
+# format understood by jekyll. Requires wget and bash installed. 
 #
 
-URL="https://raw.githubusercontent.com/wiki/hakierspejs/wiki/Jak-si%C4%99-zapisa%C4%87.md"
+URL_Apply="https://raw.githubusercontent.com/wiki/hakierspejs/wiki/Jak-si%C4%99-zapisa%C4%87.md"
+URL_FAQ="https://raw.githubusercontent.com/wiki/hakierspejs/wiki/FAQ.md"
 
-if [[ -f "./apply.md" ]]; then
+if [[ -f "./apply.md" || -f "./faq.md" ]]; then
 	rm -r "./apply.md"
 fi
 
+# apply page
 echo -e "---\ntitle: Zapisz się!\nlayout: default\n---\n" >> ./apply.md
 echo -e "## Jak się zapisać?\n" >> ./apply.md
-wget $URL -O ->> ./apply.md
+wget $URL_Apply -O ->> ./apply.md
+
+# faq page
+echo -e "---\ntitle: FAQ\nlayout: default\n---\n" >> ./faq.md
+wget $URL_FAQ -O ->> ./faq.md
